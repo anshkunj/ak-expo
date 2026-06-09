1622cb42eaf1e0e7ff1a96801fa2e0d9f810d765
anshkunj <anshkunj.ak@gmail.com>
2026-06-09 20:12:07 +0530

    config supabase for web (ssr issue solved)

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,12 +1,28 @@
 import { createClient } from '@supabase/supabase-js'
-import AsyncStorage from '@react-native-async-storage/async-storage'
+
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!
-const supabasePublishableKey = process.env.EXPO_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
-export const supabase = createClient(supabaseUrl, supabasePublishableKey, {
-  auth: {
-    storage: AsyncStorage as any,
-    autoRefreshToken: true,
-    persistSession: true,
-    detectSessionInUrl: false,
-  },
-})
+const supabaseKey = process.env.EXPO_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
+
+let supabaseInstance: ReturnType<typeof createClient> | null = null
+
+export function getSupabase() {
+  if (supabaseInstance) return supabaseInstance
+
+  const isWeb = typeof window !== 'undefined'
+
+  const storage =
+    isWeb
+      ? undefined
+      : require('@react-native-async-storage/async-storage').default
+
+  supabaseInstance = createClient(supabaseUrl, supabaseKey, {
+    auth: {
+      storage,
+      autoRefreshToken: true,
+      persistSession: true,
+      detectSessionInUrl: isWeb,
+    },
+  })
+
+  return supabaseInstance
+}
